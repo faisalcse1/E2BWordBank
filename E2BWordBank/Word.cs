557015ad7a4301362416eeba8249a56db9df3c41
faisalcse1 <faisalcse1@gmail.com>
2024-11-12 20:04:24 +0600

    Word model has been added..

---
+++ E2BWordBank/Word.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace E2BWordBank
+{
+    public class Word
+    {
+        [JsonProperty("pron")]
+        public object Pron { get; set; }
+
+        [JsonProperty("bn")]
+        public string Bn { get; set; }
+
+        [JsonProperty("en")]
+        public string En { get; set; }
+
+        [JsonProperty("bn_syns")]
+        public object BnSyns { get; set; }
+
+        [JsonProperty("en_syns")]
+        public object EnSyns { get; set; }
+
+        [JsonProperty("sents")]
+        public object Sents { get; set; }
+
+        [JsonProperty("details")]
+        public string Details { get; set; }
+    }
+}
