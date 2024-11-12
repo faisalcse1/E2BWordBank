e7fe044c3f4adb6e01952189559198003a7cdd50
faisalcse1 <faisalcse1@gmail.com>
2024-11-12 20:38:34 +0600

    all method has been implemented..

--- E2BWordBank/WordRepository.cs
+++ E2BWordBank/WordRepository.cs
@@ -3,14 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace E2BWordBank
 {
     public static class WordRepository
     {
-        
-        public static List<Word> GetAll()
+        /// <summary>
+        /// Retrieves all words from the dictionary or word database.
+        /// </summary>
+        /// <returns>A list of all words available in the dictionary.</returns>
+        public static List<Word> GetAllWords()
         {
             string wordFileName = "/E2BCData.json";
             string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
@@ -19,15 +21,35 @@ namespace E2BWordBank
             return JsonConvert.DeserializeObject<List<Word>>(jsonWords);
         }
 
-        public static List<Word> GetWordsStartingWith(string prefix, string type)
+        /// <summary>
+        /// Retrieves a list of words that start with the specified prefix and match the given type.
+        /// </summary>
+        /// <param name="prefix">The starting substring or letters to filter words by.</param>
+        /// <param name="type">The type or category of words to retrieve (e.g., En, Bn).</param>
+        /// <returns>A list of words that match the specified prefix and type.</returns>
+        public static List<Word> GetWordsStartingWith(string prefix, WordType type= WordType.En)
         {
-            var words = GetAll();
+            var words = GetAllWords();
             return words.Where(c=>
-            (type=="en"&& c.En.ToLower().Contains(prefix.ToLower()))
-            || (type == "bn" && c.Bn.Contains(prefix))
+            (type== WordType.En && c.En.ToLower().Contains(prefix.ToLower()))
+            || (type == WordType.Bn && c.Bn.Contains(prefix))
             ).ToList();
         }
 
-        
+        /// <summary>
+        /// Searches for a single word that exactly matches the specified prefix and type.
+        /// </summary>
+        /// <param name="prefix">The exact word or substring to match.</param>
+        /// <param name="type">The type or category of the word to retrieve (e.g., En, Bn). Defaults to English (WordType.En).</param>
+        /// <returns>A single word that exactly matches the given prefix and type, or null if no match is found.</returns>
+        public static Word FindWord(string prefix, WordType type = WordType.En)
+        {
+            var words = GetAllWords();
+            return words.FirstOrDefault(c =>
+            (type == WordType.En && c.En.ToLower()==prefix.ToLower())
+            ||(type == WordType.Bn && c.Bn==prefix)
+            );
+        }
+
     }
 }
