b424831d8176f425a714384daf8e30aedfa54210
faisalcse1 <faisalcse1@gmail.com>
2024-11-12 20:23:04 +0600

    WordRepository has been implemented.

--- E2BWordBank/IWordRepository.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace E2BWordBank
-{
-    public interface IWordRepository
-    {
-    }
-}
--- E2BWordBank/WordRepository.cs
+++ E2BWordBank/WordRepository.cs
@@ -1,6 +1,33 @@
-﻿namespace E2BWordBank
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace E2BWordBank
 {
-    public class WordRepository
+    public static class WordRepository
     {
+        
+        public static List<Word> GetAll()
+        {
+            string wordFileName = "/E2BCData.json";
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string wordFilePath=baseDirectory + wordFileName;
+            string jsonWords=File.ReadAllText(wordFilePath);
+            return JsonConvert.DeserializeObject<List<Word>>(jsonWords);
+        }
+
+        public static List<Word> GetWordsStartingWith(string prefix, string type)
+        {
+            var words = GetAll();
+            return words.Where(c=>
+            (type=="en"&& c.En.ToLower().Contains(prefix.ToLower()))
+            || (type == "bn" && c.Bn.Contains(prefix))
+            ).ToList();
+        }
+
+        
     }
 }
